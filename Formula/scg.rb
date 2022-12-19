2e4075168992db7e74ea93a13ac3bbb2b885e399
chyugo <1911447416@qq.com>
2022-12-19 14:02:09 +0800

    2

--- Formula/scg.rb
+++ Formula/scg.rb
@@ -1,10 +1,35 @@
-class scg < Formula
-    desc "根据模板和指定的参数自动生成文件"
-    homepage "https://github.com/chyugo/scg"
-    url "https://github.com/chyugo/scg/releases/download/scg/scg-mac.tar.gz"
-    sha256 "ce86acbf2bb172c1315dc21392f0020254a60c842ebbea2b346f84f0665602dc"
-    version "1.0"
-    def install
-      bin.install "scg"
-    end
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Scg < Formula
+  desc "scg"
+  homepage "https://github.com/chyugo/scg/"
+  url "https://github.com/chyugo/scg/releases/download/scg/scg-mac.tar.gz"
+  version "1.0"
+  sha256 "ce86acbf2bb172c1315dc21392f0020254a60c842ebbea2b346f84f0665602dc"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "acli"
   end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test scg`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
